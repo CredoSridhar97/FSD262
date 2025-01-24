c6868b7aabc1604616115aec43f57f21ade19a93
Sridhar Raj P <134199490+CredoSridhar97@users.noreply.github.com>
2025-01-24 16:49:53 +0530

    Add files via upload

---
+++ OOPS/Day1Methods.java
@@ -0,0 +1,10 @@
+public class Day1Methods {
+    static  void display()
+    {
+        System.out.println("I am Method");
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
